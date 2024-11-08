a7c713740cb9577541880bfbe2070408a6673b60
platplatformform <102703915+platplatformform@users.noreply.github.com>
2024-11-08 12:32:28 -0600

    Add files via upload

---
+++ assets/js/tags.js
@@ -0,0 +1,115 @@
+var data = [
+
+{ "Position": "1",
+  "Transcription": "Through empathy + service to others we become living monuments." 
+},
+
+{ "Position": "2",
+  "Transcription": "I question whether Monuments are a 21st century idea. I’d rather see resources devoted to public art that uplifts & is created by diverse makers." 
+},
+
+{ "Position": "3",
+  "Transcription": "(front) New, evolved monuments should be cast OVER the old ones. Like a melting candle, the new should submerge the old. The old is repudiated—PROPERLY—but not denied and not hidden." 
+},
+
+{ "Position": "4",
+  "Transcription": "(back) Logan should be covered in ooze, but remain there; atop his mass should be a more uplifting piece commemorating Black migration to our proud state of IL." 
+},
+
+{ "Position": "5",
+  "Transcription": "By telling your truth in spaces that don’t normally make space for you to be heard.<br>That’s one way of making change." 
+},
+
+{ "Position": "6",
+  "Transcription": "By helping create space for voice, memory, understanding, irony, multiplicity and gathering." 
+},
+
+{ "Position": "7",
+  "Transcription": "Black + Indigenous sovreignty [sic] in public spaces (digital + IRL)" 
+},
+
+{ "Position": "8",
+  "Transcription": "Be honest and humble when learning about our heritage—good, bad, and ugly. Take ownership for our ability to craft a new legacy." 
+},
+
+{ "Position": "9",
+  "Transcription": "Disrupting the silence—making moves, pushing for change, looking to artists to guide the way" 
+},
+
+{ "Position": "10",
+  "Transcription": "Why would we want to become the monument?" 
+},
+
+{ "Position": "11",
+  "Transcription": "By refusing to conform to arbitrary norms.<br>Fuck you I’ll wear jeans to work." 
+},
+
+{ "Position": "12",
+  "Transcription": "Growing in knowledge and empathy of others" 
+},
+
+{ "Position": "13",
+  "Transcription": "By learning, holding and sharing collective memories of radical love & community care with anyone who is willing to learn & listen <3" 
+},
+
+{ "Position": "14",
+  "Transcription": "Imagine and create new models for creating, being and living with feet planted and crown extended. Pride but also humility." 
+},
+
+{ "Position": "15",
+  "Transcription": "By making it live. Monuments become movements to ideas, + ideas have to live, and change, + grow." 
+},
+
+{ "Position": "16",
+  "Transcription": "By never forgetting, always reflecting" 
+},
+
+{ "Position": "17",
+  "Transcription": "Since monuments live in public spaces you can become the monument by occupying the space, engaging in the space, and creating a life-making futurity of the space that speaks to the needs of the community" 
+},
+
+{ "Position": "18",
+  "Transcription": "Have respect while encouraging dialogue about culture, history, and tradition." 
+},
+
+{ "Position": "19",
+  "Transcription": "By preserving memory through sharing stories" 
+},
+
+{ "Position": "20",
+  "Transcription": "platforming the voices of the oppressed" 
+},
+
+{ "Position": "21",
+  "Transcription": "We can be REALLY LOUD! And yell and wear big clothes and TAKE UP SPACE!" 
+},
+
+{ "Position": "22",
+  "Transcription": "When we acknowledge that we are no different than the people that came before us. Full of doubt and changing our truth with the pass of time." 
+},
+
+{ "Position": "23",
+  "Transcription": "LIVE A GOOD LIFE" 
+},
+
+{ "Position": "24",
+  "Transcription": "Forgive, love hard enough to make an impact on your community. Make every part of your life an art. [signature]" 
+},
+
+{ "Position": "25",
+  "Transcription": "Through building collective systems of support with one another<br>xx Abby (Bloops)" 
+},
+
+{ "Position": "26",
+  "Transcription": "By striving to see the world through a critical lens" 
+},
+
+{ "Position": "27",
+  "Transcription": "We can listen to people, we can build community, we can write down our movements, oral histories" 
+},
+
+{ "Position": "28",
+  "Transcription": "by creating change.<br>culture is knowledge<br>knowledge is everything." 
+}
+
+];
