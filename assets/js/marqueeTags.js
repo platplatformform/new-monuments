5607ad530b0c69449fde66ffc34437f3c51858e8
platplatformform <102703915+platplatformform@users.noreply.github.com>
2024-12-06 13:08:25 -0600

    Add files via upload

---
+++ assets/js/marqueeTags.js
@@ -0,0 +1,531 @@
+var marqueeData = [
+
+{ "Position": "1",
+  "Transcription": "Through empathy + service to others we become living monuments." 
+},
+
+{ "Position": "2",
+  "Transcription": "I question whether Monuments are a 21st century idea. I’d rather see resources devoted to public art that uplifts & is created by diverse makers." 
+},
+
+{ "Position": "3",
+  "Transcription": "(front) New, evolved monuments should be cast OVER the old ones. Like a melting candle, the new should submerge the old. The old is repudiated—PROPERLY—but not denied and not hidden." 
+},
+
+{ "Position": "4",
+  "Transcription": "(back) Logan should be covered in ooze, but remain there; atop his mass should be a more uplifting piece commemorating Black migration to our proud state of IL." 
+},
+
+{ "Position": "5",
+  "Transcription": "By telling your truth in spaces that don’t normally make space for you to be heard. / That’s one way of making change." 
+},
+
+{ "Position": "6",
+  "Transcription": "By helping create space for voice, memory, understanding, irony, multiplicity and gathering." 
+},
+
+{ "Position": "7",
+  "Transcription": "Black + Indigenous sovreignty [sic] in public spaces (digital + IRL)" 
+},
+
+{ "Position": "8",
+  "Transcription": "Be honest and humble when learning about our heritage—good, bad, and ugly. Take ownership for our ability to craft a new legacy." 
+},
+
+{ "Position": "9",
+  "Transcription": "Disrupting the silence—making moves, pushing for change, looking to artists to guide the way" 
+},
+
+{ "Position": "10",
+  "Transcription": "Why would we want to become the monument?" 
+},
+
+{ "Position": "11",
+  "Transcription": "By refusing to conform to arbitrary norms. / Fuck you I’ll wear jeans to work." 
+},
+
+{ "Position": "12",
+  "Transcription": "Growing in knowledge and empathy of others" 
+},
+
+{ "Position": "13",
+  "Transcription": "By learning, holding and sharing collective memories of radical love & community care with anyone who is willing to learn & listen <3" 
+},
+
+{ "Position": "14",
+  "Transcription": "Imagine and create new models for creating, being and living with feet planted and crown extended. Pride but also humility." 
+},
+
+{ "Position": "15",
+  "Transcription": "By making it live. Monuments become movements to ideas, + ideas have to live, and change, + grow." 
+},
+
+{ "Position": "16",
+  "Transcription": "By never forgetting, always reflecting" 
+},
+
+{ "Position": "17",
+  "Transcription": "Since monuments live in public spaces you can become the monument by occupying the space, engaging in the space, and creating a life-making futurity of the space that speaks to the needs of the community" 
+},
+
+{ "Position": "18",
+  "Transcription": "Have respect while encouraging dialogue about culture, history, and tradition." 
+},
+
+{ "Position": "19",
+  "Transcription": "By preserving memory through sharing stories" 
+},
+
+{ "Position": "20",
+  "Transcription": "platforming the voices of the oppressed" 
+},
+
+{ "Position": "21",
+  "Transcription": "We can be REALLY LOUD! And yell and wear big clothes and TAKE UP SPACE!" 
+},
+
+{ "Position": "22",
+  "Transcription": "When we acknowledge that we are no different than the people that came before us. Full of doubt and changing our truth with the pass of time." 
+},
+
+{ "Position": "23",
+  "Transcription": "LIVE A GOOD LIFE" 
+},
+
+{ "Position": "24",
+  "Transcription": "Forgive, love hard enough to make an impact on your community. Make every part of your life an art. [signature]" 
+},
+
+{ "Position": "25",
+  "Transcription": "Through building collective systems of support with one another / xx Abby (Bloops)" 
+},
+
+{ "Position": "26",
+  "Transcription": "By striving to see the world through a critical lens" 
+},
+
+{ "Position": "27",
+  "Transcription": "We can listen to people, we can build community, we can write down our movements, oral histories" 
+},
+
+{ "Position": "28",
+  "Transcription": "by creating change. / culture is knowledge / knowledge is everything." 
+},
+
+{ "Position": "29",
+  "Transcription": "First & foremost pay respect to the land & Indiginous [sic] community in which we occupy the space. And have representation of the breadth of citizens & immigrants & refugee people that make up the U.S. No more colonist statue." 
+},
+
+{ "Position": "30",
+  "Transcription": "by staying true to myself & who i am at my core, i am all i have" 
+},
+
+{ "Position": "31",
+  "Transcription": "The moment that need [sic] to be memorized forever. It may be wrong (consider the history), it may be excellent, but all carry the past to present." 
+},
+
+{ "Position": "32",
+  "Transcription": "Becoming free within oneself." 
+},
+
+{ "Position": "33",
+  "Transcription": "Be yourself, go find love!!! Everything you do is good. I love you. —DÜDE" 
+},
+
+{ "Position": "34",
+  "Transcription": "By preserving and taking in the world around us unapoligeticly [sic]." 
+},
+
+{ "Position": "35",
+  "Transcription": "Lead with pride without forgetting the message." 
+},
+
+{ "Position": "36",
+  "Transcription": "Show up. Slow down. Divest. Listen" 
+},
+
+{ "Position": "37",
+  "Transcription": "I would like to see more monuments of unrecognized civic folks but somehow try to educate/not alienate former hardcore/older people who upset about all this. Hope and discussion!" 
+},
+
+{ "Position": "38",
+  "Transcription": "We can perhaps do MORE than the bare minimum for others." 
+},
+
+{ "Position": "39",
+  "Transcription": "BE STRONG & REFLECT" 
+},
+
+{ "Position": "40",
+  "Transcription": "by following our dreams of promoting black ambition within institutions" 
+},
+
+{ "Position": "41",
+  "Transcription": "I’m strongly supportive of highlighting complex history. New monuments seem like a better way to do that." 
+},
+
+{ "Position": "42",
+  "Transcription": "Just keep on living, baby! ♡" 
+},
+
+{ "Position": "43",
+  "Transcription": "FLOWERIN’ / CARIN’ / Shining / SHARIN’ / Rooting / [drawing of five birds surrounding a flower]" 
+},
+
+{ "Position": "44",
+  "Transcription": "Passion for positive causes." 
+},
+
+{ "Position": "45",
+  "Transcription": "By learning to accept one another, for who we are." 
+},
+
+{ "Position": "46",
+  "Transcription": "Like Logan, we can acknowledge our errors/falsehoods + try to change history." 
+},
+
+{ "Position": "47",
+  "Transcription": "By remembering, archiving, documenting, grieving & grieving some mo [sic]" 
+},
+
+{ "Position": "48",
+  "Transcription": "Educating others and celebrating history :)" 
+},
+
+{ "Position": "49",
+  "Transcription": "about lifestyle, mind, or other somethings." 
+},
+
+{ "Position": "50",
+  "Transcription": "BY RECOGNIZING OUR STRENGTH!" 
+},
+
+{ "Position": "51",
+  "Transcription": "MAKE LOVE NOT WAR" 
+},
+
+{ "Position": "52",
+  "Transcription": "We can look forward, not back." 
+},
+
+{ "Position": "53",
+  "Transcription": "By defining our relationships to the monuments, not letting the monuments be definitional. / JV" 
+},
+
+{ "Position": "54",
+  "Transcription": "The monument needs to be in conversation with us! So—flipping the script? direction." 
+},
+
+{ "Position": "55",
+  "Transcription": "stop making ideas compete with money" 
+},
+
+{ "Position": "56",
+  "Transcription": "everyday acts of kindness :)" 
+},
+
+{ "Position": "57",
+  "Transcription": "by empowering ourselves to become the change" 
+},
+
+{ "Position": "58",
+  "Transcription": "Reflect on what has come before us, both good and bad, and how that has influenced what is today." 
+},
+
+{ "Position": "59",
+  "Transcription": "FREE PALESTINE" 
+},
+
+{ "Position": "60",
+  "Transcription": "We can be open to others + really listen, be thoughtful + be willing to change our minds if we find the things we used to believe aren’t true." 
+},
+
+{ "Position": "61",
+  "Transcription": "YOU CAN’T BE CREATIVE WITHOUT ALL FORMS OF ART. BE KIND TO ALL FORMS OF ART. =) / SUZANNE. S" 
+},
+
+{ "Position": "62",
+  "Transcription": "Spread love not hate." 
+},
+
+{ "Position": "63",
+  "Transcription": "NON Biased" 
+},
+
+{ "Position": "64",
+  "Transcription": "through dialogue and conversation" 
+},
+
+{ "Position": "65",
+  "Transcription": "Argentina + Italy + Ireland are the ♡ best inspiration / 2024" 
+},
+
+{ "Position": "66",
+  "Transcription": "We can bring people together tonite [sic]" 
+},
+
+{ "Position": "67",
+  "Transcription": "MONUMENTS THAT ARE TRULY PUBLICLY OWNED" 
+},
+
+{ "Position": "68",
+  "Transcription": "Every moment matters. Rest, action, connection, solitude—everything is important. We are our history, we are our future—everything & everyone is connected. / Love wholly and truthfully listen to a stranger’s story. Recognize that everyone is living this complex and beautiful life with you. We are monuments of all the ways that we love. ♡" 
+},
+
+{ "Position": "69",
+  "Transcription": "We build around the monument" 
+},
+
+{ "Position": "70",
+  "Transcription": "Stand in front of it and imagine you’re riding the horse. Where are you going?" 
+},
+
+{ "Position": "71",
+  "Transcription": "Gather to share stories of life, love + grief. Carry these stories with us + share them in moments of trueness." 
+},
+
+{ "Position": "72",
+  "Transcription": "learning from history / creating a new positive future." 
+},
+
+{ "Position": "73",
+  "Transcription": "LIVE" 
+},
+
+{ "Position": "74",
+  "Transcription": "By following our dreams and using them to make the world a better place" 
+},
+
+{ "Position": "75",
+  "Transcription": "Recognizing & supporting leaders making change ♡" 
+},
+
+{ "Position": "76",
+  "Transcription": "THROUGH UNDERSTANDING, COMPASSION, LOVE + TOGETHERNESS/ONENESS / KBB" 
+},
+
+{ "Position": "77",
+  "Transcription": "Be open to accepting all humans, animals and being who seek to create a culture of peace ♡" 
+},
+
+{ "Position": "78",
+  "Transcription": "GATHER AND STAND ON EACH OTHER’S SHOULDERS / [drawing of a 10 stick figures, arranged in a pyramid formation]" 
+},
+
+{ "Position": "79",
+  "Transcription": "I am not sure what can or can’t be done, but I feel like the original monument can/maybe should? see an alteration to reveal its history further!" 
+},
+
+{ "Position": "80",
+  "Transcription": "Make History" 
+},
+
+{ "Position": "81",
+  "Transcription": "Showcasing our cultural identities to the world with passion and being QUEER!" 
+},
+
+{ "Position": "82",
+  "Transcription": "By planting trees and protecting and nurturing them" 
+},
+
+{ "Position": "83",
+  "Transcription": "By being truly and fully ourselves and not letting others oppress either us or others." 
+},
+
+{ "Position": "84",
+  "Transcription": "By Remembering what we Love & Honor" 
+},
+
+{ "Position": "85",
+  "Transcription": "By being the change we want to see, leaving the world better than we found it and honoring the generations to come ?" 
+},
+
+{ "Position": "86",
+  "Transcription": "Get Inspired and Inspire others!" 
+},
+
+{ "Position": "87",
+  "Transcription": "By seeing yourself in others" 
+},
+
+{ "Position": "88",
+  "Transcription": "We can make more diverse monuments" 
+},
+
+{ "Position": "89",
+  "Transcription": "Embrace History / DON’T REPEAT IT!" 
+},
+
+{ "Position": "90",
+  "Transcription": "By celebrating where we are in the present, together!" 
+},
+
+{ "Position": "91",
+  "Transcription": "With love, lust and tangental [sic] experimentation / -EMMA MCKEE-" 
+},
+
+{ "Position": "92",
+  "Transcription": "Liberation of the oppressed. Free Palestine" 
+},
+
+{ "Position": "93",
+  "Transcription": "By dressing fabulously" 
+},
+
+{ "Position": "94",
+  "Transcription": "Through Love. In Action" 
+},
+
+{ "Position": "95",
+  "Transcription": "[drawing of the earth surrounded by 12 stick figures and 3 hearts]" 
+},
+
+{ "Position": "96",
+  "Transcription": "This is stolen land. Return this Earth to indigenous people. Indigenous land stewardship NOW!" 
+},
+
+{ "Position": "97",
+  "Transcription": "By creating an environment and space that amplifies everyone’s voice and thoughts into one large collective thought and being. A world spark" 
+},
+
+{ "Position": "98",
+  "Transcription": "[drawing of a large, smiling stick figure in the middle of four smaller, non-smiling stick figures]" 
+},
+
+{ "Position": "99",
+  "Transcription": "Smile First, Die Once and Dream Again Maybe?" 
+},
+
+{ "Position": "100",
+  "Transcription": "When we take heed to, first, the land to which the other material[s] may take claim—we make room to be like stone in who we are & what we know. / Song to remember: * Pure Gold x The Clark Sisters *" 
+},
+
+{ "Position": "101",
+  "Transcription": "Build our own and put them up!" 
+},
+
+{ "Position": "102",
+  "Transcription": "-community input / -unsung heroes" 
+},
+
+{ "Position": "103",
+  "Transcription": "Let’s use what we have around us (i.e., Logan) as prompts to discuss then, now, etc." 
+},
+
+{ "Position": "104",
+  "Transcription": "By living with integrity! / Four pillars of integrity by Jim Dethmer / -Radical Responsibility / - Feel your feelings / - Be candid about your feelings / - Be impeccable with your agreements / * I was recently inspired by these pillars" 
+},
+
+{ "Position": "105",
+  "Transcription": "Statue of dachshund Maddie." 
+},
+
+{ "Position": "106",
+  "Transcription": "Having contemporary or great people that aren’t talked about." 
+},
+
+{ "Position": "107",
+  "Transcription": "Through our actions unto others / [drawing of sun, drawing of flower]" 
+},
+
+{ "Position": "108",
+  "Transcription": "by looking out for one another / by being yourself / by sharing your story / ♡" 
+},
+
+{ "Position": "109",
+  "Transcription": "My mom is in the M2M group!!! =)" 
+},
+
+{ "Position": "110",
+  "Transcription": "KEYSHIA Scott" 
+},
+
+{ "Position": "111",
+  "Transcription": "Maurie Scott" 
+},
+
+{ "Position": "112",
+  "Transcription": "[drawing of person’s head in profile] / Raul M. / accept everyone / Deny no one" 
+},
+
+{ "Position": "113",
+  "Transcription": "By supporting / creating / + building / By dismantling / reshaping / reclaiming / responds / [drawing]" 
+},
+
+{ "Position": "114",
+  "Transcription": "By remembering / By creating / By taking up space / the monumental becomes MONUMENT / [signature]" 
+},
+
+{ "Position": "115",
+  "Transcription": "Love More – Kristoph López" 
+},
+
+{ "Position": "116",
+  "Transcription": "break through the human boundaries of flesh" 
+},
+
+{ "Position": "117",
+  "Transcription": "[drawing]" 
+},
+
+{ "Position": "118",
+  "Transcription": "Be All—IN! / MYY" 
+},
+
+{ "Position": "119",
+  "Transcription": "Acknowledge privilege and act accordingly" 
+},
+
+{ "Position": "120",
+  "Transcription": "By standing firm in our whole being; / By becoming unshakable / By owning your history / By wearing all that you are proudly ♡" 
+},
+
+{ "Position": "121",
+  "Transcription": "by holding histories accountable and imagining new forms to monumentalize no individuals but movements?" 
+},
+
+{ "Position": "122",
+  "Transcription": "By breathing very, very, very slowly and watching it disintegrate." 
+},
+
+{ "Position": "123",
+  "Transcription": "By doing monumental gifting to The Rest of Us" 
+},
+
+{ "Position": "124",
+  "Transcription": "Radical acts of kindness, cultural reclaimation [sic] + abundant Love [signature]" 
+},
+
+{ "Position": "125",
+  "Transcription": "Reflecting the actions that shape our internal dialogues / Be the image that you believe" 
+},
+
+{ "Position": "126",
+  "Transcription": "We can become a monument by being confident and considerate to our neighbors. Building growth & equity among the masses **" 
+},
+
+{ "Position": "127",
+  "Transcription": "We become the monument by tearing it down and making a fabulous new one instead" 
+},
+
+{ "Position": "128",
+  "Transcription": "Move thru this life with the understanding that you *we are a historical figure; you *we are a monument. Right now. So act like it. / ~Love, Marie (@i.am.lovemarie)" 
+},
+
+{ "Position": "129",
+  "Transcription": "GET BIG! / (TAKE UP SPACE)" 
+},
+
+{ "Position": "130",
+  "Transcription": "♡ Family is all you need ♡ / [signature]" 
+},
+
+{ "Position": "131",
+  "Transcription": "Liberty and freedom of thought and expression for all" 
+},
+
+{ "Position": "132",
+  "Transcription": "Be kind" 
+}
+
+];
